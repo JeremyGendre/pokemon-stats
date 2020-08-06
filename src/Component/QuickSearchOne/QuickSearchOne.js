59333548ebf189933e76f64be540e02d28c3b238
Jérémy Gendre <jgendre@media-sante.com>
2020-08-06 23:08:51 +0200

    FEAT : quick search ok in front + change the title and logo

--- src/Component/QuickSearchOne/QuickSearchOne.js
+++ src/Component/QuickSearchOne/QuickSearchOne.js
@@ -1,45 +1,45 @@
 import React, {Component} from "react";
+import _ from 'lodash';
 import './QuickSearchOne.css';
 import {Search} from 'semantic-ui-react';
-import {FRENCH_POKEMON_NAMES} from "../../../config/frenchPokemonNames";
+import {FRENCH_POKEMON_NAMES} from "../../config/frenchPokemonNames.js";
+
+const source = JSON.parse(FRENCH_POKEMON_NAMES);
+
+const initialState = {
+    isLoading: false,
+    results: [],
+    value: ''
+};
 
 export default class QuickSearchOne extends Component{
 
     constructor(props) {
         super(props);
-        this.state = {
-            isLoading: false,
-            results: [],
-            value: ''
-        };
+        this.state = initialState;
     }
 
-    handleResultSelect = (e, { result }) => this.setState({ value: result.title })
+    handleResultSelect = (e, { result }) => this.setState({ value: result.name })
 
     handleSearchChange = (e, { value }) => {
         this.setState({ isLoading: true, value })
 
         setTimeout(() => {
             if (this.state.value.length < 1) return this.setState(initialState)
-
-            const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
-            const isMatch = (result) => re.test(result.title)
-
-            this.setState({
-                isLoading: false,
-                results: _.filter(source, isMatch),
-            })
+            if(this.state.value.length >= 3){
+                const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+                const isMatch = (result) => re.test(result.name)
+                this.setState({
+                    isLoading: false,
+                    results: _.filter(source, isMatch),
+                })
+            }else{
+                this.setState({
+                    isLoading: false,
+                    results: [],
+                })
+            }
         }, 300)
-    }
-
-    componentDidMount() {
-        try{
-            let frenchPokemonNamesDecoded = JSON.parse(FRENCH_POKEMON_NAMES);
-            console.log(frenchPokemonNamesDecoded);
-            this.setState({results:frenchPokemonNamesDecoded});
-        }catch (e) {
-            console.log(e);
-        }
     }
 
     render(){
@@ -51,8 +51,11 @@ export default class QuickSearchOne extends Component{
                     onSearchChange={_.debounce(this.handleSearchChange, 500, {
                         leading: true,
                     })}
-                    results={results}
-                    value={value}
+                    results={this.state.results}
+                    value={this.state.value}
+                    placeholder="Rechercher un pokemon..."
+                    size="big"
+                    title="Recherche de pokemon"
                 />
             </div>
         );
