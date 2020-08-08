a0038a67dc4f64ce98216843f1efffa14747e157
Jérémy Gendre <jgendre@media-sante.com>
2020-08-08 18:12:41 +0200

    Updates

---
+++ precache-manifest.24fb0085ed488bb47febbce372e98b8f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee038d32c50593f975c1ef10f7354522",
+    "url": "/pokemon-stats/index.html"
+  },
+  {
+    "revision": "6d123d51dfb5e95fd2bd",
+    "url": "/pokemon-stats/static/css/2.2cdbc243.chunk.css"
+  },
+  {
+    "revision": "b679fcd30c752170f133",
+    "url": "/pokemon-stats/static/css/main.7d1552b5.chunk.css"
+  },
+  {
+    "revision": "6d123d51dfb5e95fd2bd",
+    "url": "/pokemon-stats/static/js/2.1b0d8993.chunk.js"
+  },
+  {
+    "revision": "9888f31f27c9e314a647d760334d609b",
+    "url": "/pokemon-stats/static/js/2.1b0d8993.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b679fcd30c752170f133",
+    "url": "/pokemon-stats/static/js/main.23a9777e.chunk.js"
+  },
+  {
+    "revision": "87752c46cdd4adfd5b3d",
+    "url": "/pokemon-stats/static/js/runtime-main.308b1547.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/pokemon-stats/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/pokemon-stats/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/pokemon-stats/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/pokemon-stats/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/pokemon-stats/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/pokemon-stats/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/pokemon-stats/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/pokemon-stats/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/pokemon-stats/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/pokemon-stats/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/pokemon-stats/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/pokemon-stats/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/pokemon-stats/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/pokemon-stats/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/pokemon-stats/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/pokemon-stats/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
