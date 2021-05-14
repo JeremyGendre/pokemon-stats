869fa836af172b65bf5b1592a876e2290df383fb
Jérémy Gendre <jgendre@media-sante.com>
2021-05-14 16:40:22 +0200

    start to switch from React classes to React function. Remove useless form and button. Now search each time a pokemon is selected from the list

--- src/Component/QuickSearchOne/QuickSearchOne.js
+++ src/Component/QuickSearchOne/QuickSearchOne.js
@@ -1,9 +1,9 @@
-import React, {Component} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import _ from 'lodash';
 import axios from 'axios';
 import {API_BASE_URL} from '../../App';
 import './QuickSearchOne.css';
-import {Button, Search, SegmentGroup} from 'semantic-ui-react';
+import {Search} from 'semantic-ui-react';
 import {FRENCH_POKEMON_NAMES} from "../../config/config.js";
 import RenderOnePokemon from "../RenderOnePokemon/RenderOnePokemon";
 import Swal from 'sweetalert2';
@@ -24,92 +24,86 @@ const initialState = {
     pokemon: null
 };
 
-export default class QuickSearchOne extends Component{
+export default function QuickSearchOne(){
+    const [state, setState] = useState(initialState);
 
-    constructor(props) {
-        super(props);
-        this.state = initialState;
+    const handleResultSelect = (e, { result }) => {
+        setState(prevState => ({ ...prevState, value: result.name, pokemonId: result.id }))
     }
 
-    handleResultSelect = (e, { result }) => {
-        this.setState({ value: result.name, pokemonId: result.id })
-    }
-
-    handleSearchChange = (e, { value }) => {
-        this.setState({ isLoading: true, value, pokemonId : null })
+    const handleSearchChange = (e, { value }) => {
+        setState(prevState => ({ ...prevState, isLoading: true, value, pokemonId : null }))
 
         setTimeout(() => {
-            if (this.state.value.length < 1) return this.setState(initialState)
-
-            const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+            const re = new RegExp(_.escapeRegExp(value), 'i')
             const isMatch = (result) => re.test(result.name)
-            this.setState({
+            setState(prevState => ({
+                ...prevState,
                 isLoading: false,
                 results: _.filter(source, isMatch),
-            })
+            }))
         }, 300)
     }
 
-    researchPokemon = (e) => {
-        e.preventDefault();
-        this.setState({isSearchButtonLoading: true});
-        if(this.state.pokemonId !== null){
-            axios.get(API_BASE_URL+'/pokemon/'+this.state.pokemonId).then((data)=>{
+    const researchPokemon = useCallback(() => {
+        setState(prevState => ({ ...prevState, isSearchButtonLoading: true}));
+        if(state.pokemonId !== null){
+            axios.get(API_BASE_URL+'/pokemon/'+state.pokemonId).then((data)=>{
                 let description = '';
                 let frenchName = '';
                 for(let i = 0; i < source.length; i++){
-                    if(source[i].id === this.state.pokemonId){
+                    if(source[i].id === state.pokemonId){
                         description = source[i].description;
                         frenchName = source[i].name;
                         break;
                     }
                 }
-                this.setState({
+                setState(prevState => ({
+                    ...prevState,
                     isSearchButtonLoading: false,
                     pokemon:data.data,
-                    frenchName: (frenchName !== '') ? frenchName : this.state.value,
+                    frenchName: (frenchName !== '') ? frenchName : state.value,
                     description: description,
-                });
+                }));
             }).catch((e)=>{
                 console.error(e);
                 MySwal.fire({icon:'error',text:'Une erreur est survenue lors de la récupération des données.'});
-                this.setState({isSearchButtonLoading: false});
+                setState(prevState => ({ ...prevState, isSearchButtonLoading: false}));
             });
         }
-    }
+    },[state.pokemonId, state.value])
 
-    render(){
-        return (
-            <div className="quick-search-container">
-                <form onSubmit={this.researchPokemon}>
-                    <Search
-                        loading={this.state.isLoading}
-                        onResultSelect={this.handleResultSelect}
-                        onSearchChange={_.debounce(this.handleSearchChange, 500, {
-                            leading: true,
-                        })}
-                        results={this.state.results}
-                        value={this.state.value}
-                        placeholder="Rechercher un pokemon..."
-                        size="big"
-                        title="Recherche de pokemon"
-                        className="quick-search-input"
-                    />
-                    <SegmentGroup className="no-border no-shadow">
-                        <Button
-                            loading={this.state.isSearchButtonLoading}
-                            inverted
-                            disabled={this.state.pokemonId === null}
-                        >Rechercher</Button>
-                    </SegmentGroup>
-                    {this.state.pokemon !== null && (
-                        <RenderOnePokemon
-                            pokemon={this.state.pokemon}
-                            frenchName={this.state.frenchName}
-                            description={this.state.description}/>
-                    )}
-                </form>
-            </div>
-        );
-    }
+    useEffect(() => {
+        if(!state.value){
+            setState(initialState);
+        }
+    },[state.value])
+    
+    useEffect(() => {
+        if(state.pokemonId){
+            researchPokemon();
+        }
+    },[researchPokemon, state.pokemonId])
+
+    return (
+        <div className="quick-search-container">
+            <Search
+                loading={state.isLoading}
+                onResultSelect={handleResultSelect}
+                onSearchChange={handleSearchChange}
+                results={state.results}
+                value={state.value}
+                placeholder="Rechercher un pokemon..."
+                size="big"
+                title="Recherche de pokemon"
+                className="quick-search-input"
+            />
+            {state.pokemon !== null && (
+                <RenderOnePokemon
+                    pokemon={state.pokemon}
+                    frenchName={state.frenchName}
+                    description={state.description}/>
+            )}
+        </div>
+    );
 }
--- src/Component/RenderOnePokemon/RenderOnePokemon.js
+++ src/Component/RenderOnePokemon/RenderOnePokemon.js
@@ -1,78 +1,74 @@
-import React, {Component, Suspense} from "react";
+import React, {Suspense} from "react";
 import './RenderOnePokemon.css';
 import {Card, Image, Loader, Table} from "semantic-ui-react";
 import {POKEMON_TYPES} from "../../config/config";
 
-export default class RenderOnePokemon extends Component{
-
-    render(){
-        const { pokemon, frenchName, description } = this.props;
-        return (
-            <div className="render-one-pokemon-container">
-                <Suspense fallback={<Loader />}>
-                    {pokemon !== null && (
-                        <Card className="custom-card-container">
-                            <Card.Content className="custom-card-content">
-                                <Image
-                                    floated='right'
-                                    size='small'
-                                    src={pokemon.sprites.front_default}
-                                    className="custom-card-image"
-                                />
-                                <Card.Header className="text-center">{frenchName}</Card.Header>
-                                <Card.Meta className="text-center">{description}</Card.Meta>
-                                <div className="card-header-simple-text">
-                                    <table className="card-type-table">
-                                        <tbody>
-                                        {pokemon.types.map((element,index) => {
-                                            let bgColor = 'white';
-                                            let translatedTypeName = element.type.name;
-                                            for(let i = 0; i < POKEMON_TYPES.length; i++){
-                                                if(POKEMON_TYPES[i].name === element.type.name){
-                                                    bgColor = POKEMON_TYPES[i].color;
-                                                    translatedTypeName = POKEMON_TYPES[i].translate;
-                                                    break;
-                                                }
+export default function RenderOnePokemon({ pokemon, frenchName, description }){
+    return (
+        <div className="render-one-pokemon-container">
+            <Suspense fallback={<Loader />}>
+                {pokemon !== null && (
+                    <Card className="custom-card-container">
+                        <Card.Content className="custom-card-content">
+                            <Image
+                                floated='right'
+                                size='small'
+                                src={pokemon.sprites.front_default}
+                                className="custom-card-image"
+                            />
+                            <Card.Header className="text-center">{frenchName}</Card.Header>
+                            <Card.Meta className="text-center">{description}</Card.Meta>
+                            <div className="card-header-simple-text">
+                                <table className="card-type-table">
+                                    <tbody>
+                                    {pokemon.types.map((element,index) => {
+                                        let bgColor = 'white';
+                                        let translatedTypeName = element.type.name;
+                                        for(let i = 0; i < POKEMON_TYPES.length; i++){
+                                            if(POKEMON_TYPES[i].name === element.type.name){
+                                                bgColor = POKEMON_TYPES[i].color;
+                                                translatedTypeName = POKEMON_TYPES[i].translate;
+                                                break;
                                             }
+                                        }
+                                        return (
+                                            <tr key={"pokemon-type-"+index}>
+                                                <td>
+                                                    <div style={{display:'flex'}}>
+                                                        <span className="span-types" style={{backgroundColor:bgColor, margin:'auto'}}/>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div style={{display:'flex'}}>
+                                                        <div style={{margin:'auto 0.5em'}}>{translatedTypeName}</div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        );
+                                    })}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </Card.Content>
+                        <Card.Content>
+                            <Card.Description>
+                                <Table celled selectable>
+                                    <Table.Body>
+                                        {pokemon.stats.map((stat,index)=>{
                                             return (
-                                                <tr key={"pokemon-type-"+index}>
-                                                    <td>
-                                                        <div style={{display:'flex'}}>
-                                                            <span className="span-types" style={{backgroundColor:bgColor, margin:'auto'}}/>
-                                                        </div>
-                                                    </td>
-                                                    <td>
-                                                        <div style={{display:'flex'}}>
-                                                            <div style={{margin:'auto 0.5em'}}>{translatedTypeName}</div>
-                                                        </div>
-                                                    </td>
-                                                </tr>
+                                                <Table.Row key={'pokemon-stats-' + index}>
+                                                    <Table.Cell className="text-bold">{stat.stat.name}</Table.Cell>
+                                                    <Table.Cell>{stat.base_stat}</Table.Cell>
+                                                </Table.Row>
                                             );
                                         })}
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </Card.Content>
-                            <Card.Content>
-                                <Card.Description>
-                                    <Table celled selectable>
-                                        <Table.Body>
-                                            {pokemon.stats.map((stat,index)=>{
-                                                return (
-                                                    <Table.Row key={'pokemon-stats-' + index}>
-                                                        <Table.Cell className="text-bold">{stat.stat.name}</Table.Cell>
-                                                        <Table.Cell>{stat.base_stat}</Table.Cell>
-                                                    </Table.Row>
-                                                );
-                                            })}
-                                        </Table.Body>
-                                    </Table>
-                                </Card.Description>
-                            </Card.Content>
-                        </Card>
-                    )}
-                </Suspense>
-            </div>
-        );
-    }
+                                    </Table.Body>
+                                </Table>
+                            </Card.Description>
+                        </Card.Content>
+                    </Card>
+                )}
+            </Suspense>
+        </div>
+    );
 }
